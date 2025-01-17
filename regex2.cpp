9d14a852a5ab61331b8338e55c0665d35ba3bac3
Glenn G. Chappell <ggchappell@alaska.edu>
2025-01-16 22:22:17 -0900

    Add regex2.cpp

---
+++ regex2.cpp
@@ -0,0 +1,48 @@
+// regex2.cpp
+// Glenn G. Chappell
+// 2025-01-16
+//
+// For CS 331 Spring 2025
+// Demo of regular expressions in C++
+
+#include <iostream>
+using std::cout;
+using std::cin;
+#include <string>
+using std::string;
+using std::getline;
+#include <regex>
+using std::regex;
+using std::smatch;
+using std::regex_search;
+
+
+// Main program
+int main()
+{
+    regex re1("ab*c");  // Our regular expression
+
+    // Read stdin & print info on each line matched by above regex.
+    while (true)
+    {
+        string line;
+        getline(cin, line);
+
+        if (!cin)
+        {
+            if (cin.eof())
+                break;
+            cout << "*** ERROR READING STD INPUT\n";
+            break;
+        }
+
+        smatch results;
+        // Below, if the results are not needed, then we can just do
+        //   if (regex_search(line, re1))
+        if (regex_search(line, results, re1))  // results is "out" param
+        {
+            cout << line << ": MATCHED [" << results[0] << "]\n";
+        }
+    }
+}
+
