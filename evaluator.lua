d6a322215ffe13379c6d712fe28ecb24a283242e
Glenn G. Chappell <ggchappell@alaska.edu>
2025-04-04 15:25:53 -0800

    evaluator.lua: FINISH - write func evaluator.eval

--- evaluator.lua
+++ evaluator.lua
@@ -1,6 +1,7 @@
--- evaluator.lua  UNFINISHED
+-- evaluator.lua
 -- Glenn G. Chappell
--- 2025-04-03
+-- Started: 2025-04-03
+-- Updated: 2025-04-04
 --
 -- For CS 331 Spring 2025
 -- Evaluator for Arithmetic Expression AST (rdparser3.lua format)
@@ -26,8 +27,67 @@ local SIMPLE_VAR = 3
 --
 -- Example of a simple tree-walk interpreter.
 function evaluator.eval(ast, vars)
-    -- TODO: WRITE THIS!!!
-    return 42  -- DUMMY
+    local result, literal, varname, op, arg1_ast, arg2_ast,
+          arg1_val, arg2_val
+
+    assert(type(ast) == "table")
+    assert(type(vars) == "table")
+
+    assert(#ast >= 1)
+    if ast[1] == NUMLIT_VAL then        -- Numeric literal
+        assert(#ast == 2)
+        literal = ast[2]
+        assert(type(literal) == "string")
+        result = tonumber(literal)
+        assert(type(result) == "number")
+
+    elseif ast[1] == SIMPLE_VAR then    -- Simple variable
+        assert(#ast == 2)
+        varname = ast[2]
+        assert(type(varname) == "string")
+        result = vars[varname]
+        if result == nil then  -- Undefined variable
+            result = 0
+        end
+        assert(type(result) == "number")
+
+    elseif type(ast[1] == "table") then  -- Operator
+        assert(#ast == 3)
+        assert(#ast[1] == 2)
+        assert(ast[1][1] == BIN_OP)
+
+        op = ast[1][2]
+        assert(type(op) == "string")
+
+        arg1_ast = ast[2]
+        assert(type(arg1_ast) == "table")
+        arg1_val = evaluator.eval(arg1_ast, vars)
+        assert(type(arg1_val) == "number")
+
+        arg2_ast = ast[3]
+        assert(type(arg2_ast) == "table")
+        arg2_val = evaluator.eval(arg2_ast, vars)
+        assert(type(arg2_val) == "number")
+
+        if op == "+" then
+            result = arg1_val + arg2_val
+        elseif op == "-" then
+            result = arg1_val - arg2_val
+        elseif op == "*" then
+            result = arg1_val * arg2_val
+        elseif op == "/" then
+            result = arg1_val / arg2_val  -- Let Lua floating-point
+                                          --  handle div by zero, etc.
+        else
+            assert(false, "AST is not in proper format")
+        end
+        assert(type(result) == "number")
+
+    else
+        assert(false, "AST is not in proper format")
+    end
+
+    return result
 end
 
 
